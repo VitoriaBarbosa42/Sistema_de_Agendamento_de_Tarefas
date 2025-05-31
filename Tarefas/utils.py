bdff6629a7422becb4ad0bb771575621795c94bc
VitoriaBarbosa42 <vitoria42.barbosa@gmail.com>
2025-05-31 13:29:58 -0300

    Segundo commit: Desenvolvi o restante das funções e iniciei a logica do dicionario, mas ainda não esta funcionando

--- Tarefas/utils.py
+++ Tarefas/utils.py
@@ -1,1 +1,43 @@
 # Funções utilitárias (ex: uma função para validar entrada do usuário, ou para exibir tarefas formatadas)
+
+def menu_principal():
+    print ('BEM VINDO AO GERENCIADOR DE TAREFAS :)')
+    print('_________________________________________')
+
+    while True:
+        print('Escolha uma opção abaixo: ')
+        inicio = input('[1] Criar novas tarefas [2] Ver tarefas [3] Atualizar tarefa [4] Excluir tarefas [5] Sair')
+        if len(inicio) == 0:
+            print('Digite um valor para continuar')
+        elif len(inicio) == 1 and inicio in '12345':
+            if inicio == '1':
+                inicio = '1'
+                print(f'Opção escolhida: Criar novas tarefas')
+                return inicio
+            elif inicio == '2':
+                inicio = '2'
+                print(f'Opção escolhida: Ver tarefas')
+                return inicio
+            elif inicio == '3':
+                inicio = '3'
+                print(f'Opção escolhida: Atualizar tarefa')
+                return inicio
+            elif inicio == '4':
+                inicio = '4'
+                print(f'Opção escolhida: Excluir tarefas')
+                return inicio
+            elif inicio == '5':
+                inicio = '5'
+                print(f'Opção escolhida: Sair')
+                return inicio            
+        else:
+            print('Opção invalida. Tente novamente')
+            print()
+
+def solicitat_id():
+    try:    
+        id = int(input('Digite o ID que você gostaria de condultar: '))
+        return id
+    except ValueError:
+        print ('ID invalido')
+
--- dicionarios.py
+++ dicionarios.py
@@ -3,4 +3,32 @@
 
 #chave - ID incremental : Valor -valor é outro dicionário
 
-lista_tarefas = {}
+from Tarefas import menu_principal
+from Tarefas import criar
+from Tarefas import ler
+from Tarefas import atualizar
+from Tarefas import excluir
+from Tarefas import solicitat_id
+
+while True:
+    inicio = menu_principal()
+    lista_tarefas = {}
+
+    if inicio == '1':
+        lista_tarefas = criar()
+
+    elif inicio == '2':
+        print(ler(lista_tarefas))
+        
+    elif inicio == '3':
+        id = solicitat_id
+        atualizar(lista_tarefas, id)
+
+
+    elif inicio == '4':
+        id = solicitat_id
+        excluir()
+
+    elif '5':
+        print('ATÉ A PROXIMA! :)')
+        break
